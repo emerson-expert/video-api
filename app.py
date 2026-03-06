e9fe139b1a9eb9c9b439deb5605eed6631faea0a
emerson-expert <58924847+emerson-expert@users.noreply.github.com>
2026-03-06 15:41:23 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -11,25 +11,18 @@ def home():
 def extract():
     url = request.args.get("url")
 
-    if not url:
-        return jsonify({"error": "URL obrigatória"}), 400
-
     ydl_opts = {
         "quiet": True
     }
 
-    try:
-        with YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=False)
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
 
-        return jsonify({
-            "title": info.get("title"),
-            "thumbnail": info.get("thumbnail"),
-            "video_url": info.get("url")
-        })
-
-    except Exception as e:
-        return jsonify({"error": str(e)})
+    return jsonify({
+        "title": info.get("title"),
+        "thumbnail": info.get("thumbnail"),
+        "video_url": info["url"]
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
